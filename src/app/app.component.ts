d5a2ffac29722d04a83ad3cdf6731c02f135d048
Hao <hao@glowwormonline.com>
2022-10-11 17:35:45 +0800

    1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { from } from 'rxjs';
+import { ProductServiceClient } from 'src/assets/jspb/Product.serviceServiceClientPb';
+import { Product } from 'src/assets/jspb/product_pb';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'demo-grpc';
+  service = new ProductServiceClient('https://localhost:8443');
+  constructor() {}
+
+  create = () => {
+    const p = new Product();
+    p.setUuid('' + new Date().getTime());
+    p.setName('angular create');
+    from(this.service.create(p, {})).subscribe((g) => console.log(g));
+  };
 }
---
+++ src/assets/jspb/product.service_pb.d.ts
@@ -0,0 +1,5 @@
+import * as jspb from 'google-protobuf'
+
+import * as product_pb from './product_pb';
+
+
