f8d8781dd9d8ef4c7c1a9b9b37ba620d39a3c86c
Hao <hao@glowwormonline.com>
2022-10-12 17:42:33 +0800

    grpc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { from } from 'rxjs';
+import { from, map, Observable } from 'rxjs';
 import { ProductServiceClient } from 'src/assets/jspb/Product.serviceServiceClientPb';
 import { Product } from 'src/assets/jspb/product_pb';
 
@@ -9,13 +9,45 @@ import { Product } from 'src/assets/jspb/product_pb';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  service = new ProductServiceClient('https://localhost:8443');
-  constructor() {}
+  service = new ProductServiceClient('http://localhost:9090');
+  number = 0;
+
+  xx: Product[] = [];
+  constructor() {
+    this.list().subscribe((pp) => (this.xx = pp));
+  }
 
   create = () => {
     const p = new Product();
     p.setUuid('' + new Date().getTime());
     p.setName('angular create');
-    from(this.service.create(p, {})).subscribe((g) => console.log(g));
+    from(this.service.create(p, {})).subscribe((g) =>
+      console.log(g.toObject())
+    );
+  };
+
+  list = (): Observable<Product[]> => {
+    const p = new Product();
+
+    return from(this.service.list(p, {})).pipe(map((g) => g.getListList()));
+  };
+
+  get = () => {
+    const p = new Product();
+    p.setUuid('GGG');
+    from(this.service.get(p, {})).subscribe((g) => console.log(g.toObject()));
+  };
+
+  interview = () => {
+    const p = new Product();
+    const stream = this.service.subscribe(p, {});
+
+    const o = new Observable((observe) => {
+      stream.on('data', (g) => {
+        observe.next(g.toObject());
+      });
+    });
+
+    o.subscribe((g) => console.log(g));
   };
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
+    HttpClientModule,
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
